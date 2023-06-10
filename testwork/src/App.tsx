41e71fe9a9b9d665605fdcd4b94c7fa73134910f
root <root@DESKTOP-CU96T2U>
2023-06-10 15:46:18 +0100

    MS commit

--- testwork/src/App.tsx
+++ testwork/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./navbar";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="navbar">find a carer</div>
+        <div className="navbar">for carers</div>
+        <div className="navbar">for business partners</div>
+        <div className="navbar">login</div>
+        <div className="navbar">share your needs</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ testwork/src/navbar.tsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <header className="navbar-header">
+        <div className="navbar">find carer</div>
+        <div className="navbar">for carers</div>
+        <div className="navbar">for business partners</div>
+        <div className="navbar">login</div>
+        <div className="navbar">share your needs</div>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
